a4e72b59a6f8115790b866dae4d113b6bf92efc6
Taosha Fan <fantaosha@gmail.com>
2025-09-09 15:18:40 +0000

    updated scripts for fitting and dense keypoints

---
+++ scripts/launch_fitting.py
@@ -0,0 +1,31 @@
+import subprocess
+import argparse
+from typing import List
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Mesh Fitting")
+    parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--seqs", type=str, required=True)
+    parser.add_argument("--exps", type=str, required=True)
+
+    args = parser.parse_args()
+
+    seqs: List[str] = args.seqs.split(",")
+    dataset: str = args.dataset
+    exps: str = args.exps.split(",")
+
+    command = ["python", "fitting.py"]
+
+    exp_args = [f"+experiment=final_atlas/{exp}" for exp in exps]
+
+    for seq in seqs:
+        print(f"Start fitting {seq}.")
+        common_args = [f"dataset={dataset}", f"sequence={seq}"]
+        for exp_arg in exp_args:
+            subprocess.run(command + [exp_arg] + common_args)
+        print(f"Fitted {seq}.")
+
+
+if __name__ == "__main__":
+    main()
